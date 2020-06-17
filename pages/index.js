2b90eb32e4fa0b7b2e8f6835b0e87d59b3a055ef
kokou kpadenou <kokou2kpadenou@gmail.com>
2020-06-16 23:17:11 -0400

    rename allposts.js to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,79 @@
-import Head from "next/head";
-import { Component } from "react";
-import { attributes, react as HomeContent } from "../content/home.md";
+import Link from "next/link";
+import { getAllPosts } from "../lib/api";
 
-export default class Home extends Component {
-    render() {
-        let { title, cats } = attributes;
-        return (
-            <>
-                <Head>
-                    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-                </Head>
-                <article>
-                    <h1>{title}</h1>
-                    <HomeContent />
-                    <ul>
-                        {cats.map((cat, k) => (
-                            <li key={k}>
-                                <h2>{cat.name}</h2>
-                                <p>{cat.description}</p>
-                            </li>
-                        ))}
-                    </ul>
-                </article>
-            </>
-        );
-    }
+export async function getStaticProps() {
+    const posts = getAllPosts([
+        "slug",
+        "title",
+        "date",
+        "published",
+        "tags",
+        "cover_image",
+        "description"
+    ]);
+
+    return { props: { posts } };
 }
+
+function Allposts({ posts }) {
+    //
+    return (
+        <ul>
+            {posts.map((post, id) => (
+                <li key={id}>
+                    <Link href="/posts/[slug]" as={`/posts/${post.slug}`}>
+                        <a>
+                            {post.cover_image && (
+                                <div
+                                    className="cover_img"
+                                    style={{
+                                        backgroundColor: "red",
+                                        backgroundImage: `url("${post.cover_image}")`
+                                    }}
+                                ></div>
+                            )}
+                            <div className="details">
+                                <h1>{post.title}</h1>
+                                <div>{post.description}</div>
+                            </div>
+                        </a>
+                    </Link>
+                </li>
+            ))}
+            <style jsx>{`
+                //
+                ul {
+                    list-style: none;
+                }
+
+                li {
+                    max-width: 38rem;
+                    margin: 2rem auto;
+                    //border-radius: 10px;
+                    border: 1px solid #000;
+                }
+                a {
+                    text-decoration: none;
+                    display: block;
+                }
+                a:link,
+                a:visited {
+                    color: #000;
+                }
+                .cover_img {
+                    height: 160px;
+                    background-size: cover;
+                    background-repeat: no-repeat;
+                    background-position: center;
+                    //border-top-right-radius: 10px;
+                    //border-top-left-radius: 10px;
+                }
+                .details {
+                    padding: 2rem;
+                }
+            `}</style>
+        </ul>
+    );
+}
+
+export default Allposts;
