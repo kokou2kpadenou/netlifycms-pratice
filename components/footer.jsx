26235dcf2fabfdece3f23e7e1380e4a9ad2120f4
kokou kpadenou <kokou2kpadenou@gmail.com>
2020-06-20 00:49:07 -0400

    footer component

---
+++ components/footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p>Netlify CMS Demo</p>
+            <style jsx>{`
+                footer {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    background-color: #000;
+                    color: #fff;
+                    height: 20rem;
+                }
+            `}</style>
+        </footer>
+    );
+};
+
+export default Footer;
