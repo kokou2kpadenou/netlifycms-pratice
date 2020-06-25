501fab4645e48816e24232573fd28f545df3098a
kokou kpadenou <kokou2kpadenou@gmail.com>
2020-06-25 14:37:09 -0400

    update layout to add head

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import Head from "next/head";
 import Header from "./header";
 import Main from "./main";
 import Footer from "./footer";
@@ -5,6 +7,17 @@ import Footer from "./footer";
 const Layout = ({ children, post }) => {
     return (
         <>
+            <Head>
+                <title>Netlify CMS demo with nextjs</title>
+                <meta
+                    name="viewport"
+                    content="initial-scale=1.0, width=device-width"
+                />
+                <meta
+                    name="description"
+                    content="Netlify CMS demo with nextjs."
+                />
+            </Head>
             <Header />
             <Main post={post}>{children}</Main>
             <Footer />
