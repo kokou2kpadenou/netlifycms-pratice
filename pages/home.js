6f637ea9ef8d9bfbf56b2d27453917ff85375ebd
kokou kpadenou <kokou2kpadenou@gmail.com>
2020-06-25 15:14:19 -0400

    for preview testing

--- pages/home.js
+++ pages/home.js
@@ -6,6 +6,9 @@ export default class Home extends Component {
         let { title, cats } = attributes;
         return (
             <>
+                <Head>
+                    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+                </Head>
                 <article>
                     <h1>{title}</h1>
                     <HomeContent />
