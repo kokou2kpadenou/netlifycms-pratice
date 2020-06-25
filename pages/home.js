4af70a932119663fb34df477f4a966c19a6f719c
kokou kpadenou <kokou2kpadenou@gmail.com>
2020-06-25 14:35:20 -0400

    for test only

--- pages/home.js
+++ pages/home.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import { Component } from "react";
 import { attributes, react as HomeContent } from "../content/home.md";
 
@@ -7,9 +6,6 @@ export default class Home extends Component {
         let { title, cats } = attributes;
         return (
             <>
-                <Head>
-                    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-                </Head>
                 <article>
                     <h1>{title}</h1>
                     <HomeContent />
