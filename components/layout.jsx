52badfec5d86efdbb475d63b7652c88749dbabc8
Kokou Kpadenou <kokou2kpadenou@gmail.com>
2021-12-09 09:59:50 -0500

    fix all eslint error

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,29 +1,25 @@
 import React from "react";
 import Head from "next/head";
+import Script from "next/script";
 import Header from "./header";
 import Main from "./main";
 import Footer from "./footer";
 
 const Layout = ({ children, post }) => {
-    return (
-        <>
-            <Head>
-                <title>Netlify CMS demo with nextjs</title>
-                <meta
-                    name="viewport"
-                    content="initial-scale=1.0, width=device-width"
-                />
-                <meta
-                    name="description"
-                    content="Netlify CMS demo with nextjs."
-                />
-                <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
-            </Head>
-            <Header />
-            <Main post={post}>{children}</Main>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Netlify CMS demo with nextjs</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Netlify CMS demo with nextjs." />
+        <script />
+      </Head>
+      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+      <Header />
+      <Main post={post}>{children}</Main>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
--- pages/home.js
+++ pages/home.js
@@ -1,28 +1,26 @@
-import Head from "next/head";
+import Script from "next/script";
 import { Component } from "react";
 import { attributes, react as HomeContent } from "../content/home.md";
 
 export default class Home extends Component {
-    render() {
-        let { title, cats } = attributes;
-        return (
-            <>
-                <Head>
-                    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-                </Head>
-                <article>
-                    <h1>{title}</h1>
-                    <HomeContent />
-                    <ul>
-                        {cats.map((cat, k) => (
-                            <li key={k}>
-                                <h2>{cat.name}</h2>
-                                <p>{cat.description}</p>
-                            </li>
-                        ))}
-                    </ul>
-                </article>
-            </>
-        );
-    }
+  render() {
+    let { title, cats } = attributes;
+    return (
+      <>
+        <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+        <article>
+          <h1>{title}</h1>
+          <HomeContent />
+          <ul>
+            {cats.map((cat, k) => (
+              <li key={k}>
+                <h2>{cat.name}</h2>
+                <p>{cat.description}</p>
+              </li>
+            ))}
+          </ul>
+        </article>
+      </>
+    );
+  }
 }
